3bc81302130dd64d0dd709e7d9b372c491aabc08
PhamTrung99 <teotu22@gmail.com>
2021-05-12 17:08:00 +0700

    commit

--- controllers/chatbot.controller.js
+++ controllers/chatbot.controller.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 const getHomePage = (req, res) => {
     res.send("Hello");
 };
 
 const getWebHook = (req, res) => {
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-
-    // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
@@ -34,8 +32,13 @@ const postWebHook = (req, res) => {
     let body = req.body;
     if (body.object === 'page') {
         body.entry.forEach((entry) => {
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         })
         res.status(200).send('EVENT_RECEIVED');
     } else {
@@ -43,4 +46,19 @@ const postWebHook = (req, res) => {
     }
 }
 
-module.exports = { getHomePage, getWebHook,postWebHook };
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
+
+module.exports = { getHomePage, getWebHook, postWebHook };
