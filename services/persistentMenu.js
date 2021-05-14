7d38aeba7b2f4f07f710a2999520ec0d897bac79
PhamTrung99 <teotu22@gmail.com>
2021-05-14 08:27:42 +0700

    commit

---
+++ services/persistentMenu.js
@@ -0,0 +1,34 @@
+const request = require("request")
+const sendApi = require("./sendApi")
+require('dotenv').config()''
+
+module.exports = persistentMenu = () =>{
+    const url = `https://graph.facebook.com/v10.0/me/messenger_profile?access_token=${process.env.PAGE_ACCESS_TOKEN}`
+    const request_body = {
+        "persistent_menu": [
+            {
+                "locale": "default",
+                "composer_input_disabled": false,
+                "call_to_actions": [
+                    {
+                        "type": "postback",
+                        "title": "Talk to an agent",
+                        "payload": "CARE_HELP"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Outfit suggestions",
+                        "payload": "CURATION"
+                    },
+                    {
+                        "type": "web_url",
+                        "title": "Shop now",
+                        "url": "https://www.originalcoastclothing.com/",
+                        "webview_height_ratio": "full"
+                    }
+                ]
+            }
+        ]
+    }
+    sendApi(url,request_body)
+}
